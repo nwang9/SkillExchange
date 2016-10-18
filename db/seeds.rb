b181e6d7676b39801371b57a5643ed176b146e94
nwang9 <nicowang9@gmail.com>
2016-10-18 13:53:37 -0500

    everything but components

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Nico", last_name: "Wang", email: "nicowang9@gmail.com", age: '22', password:  "password")
+
+3.times do
+	User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password:  Faker::Name.name, age: rand(1..50))
+end
+
+categories = ["Music", "Sports", "Software", "Cooking", "Home Improvement", "Other"]
+
+categories.each do |category|
+	Category.create(name: category)
+end
+
+Category.all.each do |category|
+	Skill.create(name: Faker::Name.name, user_id: rand(1..2) ,skiller_id: rand(3..4),  category_id: category.id)
+end
+
+User.all.each do |user|
+	Review.create(title: Faker::Name.name, body: Faker::Lorem.words(6), user_id: user.id, reviewer_id: rand(1..4))
+end
